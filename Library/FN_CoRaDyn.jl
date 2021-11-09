1bfe134ea2d8234b87d55ad93cc7d6f5aa19a509
mgschiavon <mgschiavon@gmail.com>
2021-11-09 12:05:50 -0600

    Renaming DY as CoRa
    And removing unnecessary functions.

--- Library/FN_CoRaDyn.jl
+++ Library/FN_CoRaDyn.jl
@@ -1,4 +1,4 @@
-# Steady state & DY calculation functions
+# Steady state & CoRa calculation functions
 #		Julia v.1.5.3
 
 module fn
@@ -132,99 +132,16 @@ module fn
 		return xD
 	end;
 
-	# DY function
+	# CoRa function
 	# INPUT: Y    - Output of full system before perturbation
 	#        YD   - Output of full system after perturbation
 	#        Ynf  - Output of non-feedback system before perturbation (Ynf:=Y)
 	#        YnfD - Output of non-feedback system after perturbation
 	# OUPUT:      - DY value
-	function DY(Y,YD,Ynf,YnfD)
+	function CoRa(Y,YD,Ynf,YnfD)
 		if abs(log10(YnfD/Ynf)) < 1e-4
 			return NaN
 		end
 		return log10(YD/Y)/log10(YnfD/Ynf);
-	end;
-
-	# DY curve
-	# INPUT: p    - Dictionary function with the ODE parameters & values
-	#        pert - Handle for the perturbation details
-	#        mm   - Handle for the considered motif
-	#        x0FB - Vector of initial state of the ODE feedback system
-	#        x0NF - Vector of initial state of the ODE no-feedback system
-	# OUPUT: DYs  - Vector of DY values for the range of parameters
-	function DYc(p,pert,mm,x0FB,x0NF)
-		r = 10 .^ collect(pert.r[1]:pert.s:pert.r[2]);
-		DYs = Array{Float64}(undef,length(r)) .+ Inf;
-		for i in 1:length(r)
-			p[pert.c] *= r[i];
-			try
-				ssR, soR = fn.RefSS(mm,p,pert,x0FB,x0NF);
-				ssD, soD = fn.PerSS(mm,p,pert,ssR,soR);
-				DYs[i] = fn.DY(mm.outFB(ssR), mm.outFB(ssD), mm.outNF(soR), mm.outNF(soD));
-			catch err
-				println("WARNING: Error in ODE simulation: <<",err,">>. CoRa --> NaN")
-				DYs[i] = NaN;
-			end
-			p[pert.c] /= r[i];
-		end
-		return DYs
-	end;
-
-	# DY "metrics"
-	# INPUT: DYs   - Vector of DY values for the range of parameters
-	#        pert  - Handle for the perturbation details
-	# OUPUT:       - [Range of DY<=eps,start,end,min(DY),optimal rho]
-	function DYm(DYs,pert)
-		r = 10 .^ collect(pert.r[1]:pert.s:pert.r[2]);
-		i = DYs .<= pert.eps;
-		j = findall(i);
-		x = copy(DYs);
-		x[x .=== NaN] .= Inf;
-		if isempty(j)
-			return [sum(DYs[i])./length(DYs[i]), NaN, NaN, minimum(x), r[argmin(x)]]
-		end
-		return [sum(DYs[i])./length(DYs[i]), r[j[1]], r[j[end]], minimum(x), r[argmin(x)]]
-	end;
-
-	# SSs for "optimal" control
-	# INPUT: p     - Dictionary function with the ODE parameters & values
-	#        pert  - Handle for the perturbation details
-	#        motif - Handle for the considered motif
-	#        DYs   - Vector of DY values for the range of parameters
-	#        uns  - 1 to use a slower, more stable ODE solver
-	# OUPUT:       - [Optimal rho, steady state for the full system before & after perturbation, and for the non-feedback system before & after perturbation]
-	function SSopt(p,pert,motif,DYs,uns)
-		r = 10 .^ collect(pert.r[1]:pert.s:pert.r[2]);
-		x = copy(DYs);
-		x[x .=== NaN] .= Inf;
-		p[pert.p] = pert.c;
-		p[pert.p] *= r[argmin(x)];
-			rtol = 1e-6;
-			ssR = zeros(length(motif.odeFB.syms));
-			soR = zeros(length(motif.odeNF.syms));
-			while(rtol >= 1e-24)
-				# Reference steady state:
-				ssR = SS(motif.odeFB, p, ssR, rtol, uns);
-				# Locally analogous system reference steady state:
-				motif.localNF(p,ssR);
-				soR = SS(motif.odeNF, p, soR, rtol, uns);
-				if(abs(motif.outFB(ssR) - motif.outNF(soR)) > 1e-4)
-					rtol *= 1e-3;
-					if(rtol < 1e-24)
-						println("ERROR: Check NF system (reltol=",rtol*1e3,").")
-						println(vcat(pert.p,i,[p[i] for i in motif.odeFB.params],motif.outFB(ssR),motif.outNF(soR)))
-						#throw(DomainError("x-("))
-					end
-				else
-					break
-				end
-			end
-			# Perturbation:
-			p[pert.p] *= pert.d;
-			ssD = SS(motif.odeFB, p, ssR, rtol, uns);
-			soD = SS(motif.odeNF, p, soR, rtol, uns);
-		p[pert.p] /= pert.d;
-		p[pert.p] /= r[argmin(x)];
-		return [vcat(p[pert.p] * r[argmin(x)],ssR,ssD,soR,soD)]
 	end;
 end
