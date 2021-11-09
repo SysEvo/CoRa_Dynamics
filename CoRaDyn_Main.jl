4c6c475b2e6ecf9e8e50375e9fe061a59c9b5736
mgschiavon <mgschiavon@gmail.com>
2021-11-09 12:01:15 -0600

    Remove unnecessary functions

--- CoRaDyn_Main.jl
+++ CoRaDyn_Main.jl
@@ -21,23 +21,8 @@ include(string("InputFiles\\ARGS_",iARG.mm,"_Par_",iARG.ex,".jl"))	# Core parame
 pO = copy(p);
 
 ## Run analysis
-# Calculate CoRa curve for a range of parameters:
-if(iARG.an=="ExSSs")
-	p = copy(pO);
-	open(string("OUT_ExSSs_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
-		writedlm(io, [vcat(iARG.ax,[string("FbR_",i) for i in mm.odeFB.syms],[string("FbD_",i) for i in mm.odeFB.syms],[string("NfR_",i) for i in mm.odeNF.syms],[string("NfD_",i) for i in mm.odeNF.syms],string("CoRa(",iARG.pp,")"))],'\t');
-		r = 10 .^ collect(pert.r[1]:pert.s:pert.r[2]);
-        for i in 1:length(r)
-			p[pert.c] *= r[i];
-			ssR, soR = fn.RefSS(mm,p,pert,x0FB,x0NF);
-			ssD, soD = fn.PerSS(mm,p,pert,ssR,soR);
-			DYs = fn.DY(mm.outFB(ssR), mm.outFB(ssD), mm.outNF(soR), mm.outNF(soD));
-			writedlm(io, [vcat(p[pert.c],ssR,ssD,soR,soD,DYs)],'\t');
-			p[pert.c] /= r[i];
-        end
-	end
 # Calculate dynamic response after a perturbation:
-elseif(iARG.an=="ExDyn")
+if(iARG.an=="ExDyn")
 	p = copy(pO);
 	open(string("OUT_ExDyn_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
 		writedlm(io, [vcat("FB","rho","time",[string(i) for i in mm.odeNF.syms])],'\t');
@@ -84,117 +69,6 @@ elseif(iARG.an=="ExDyn")
 		writedlm(io, [vcat(0,p[iARG.pp],"Inf",soD)],'\t');
 		p[pert.p] /= pert.d;
 	end
-# Calculate CoRa curve for a range of parameters as another parameter varies:
-elseif(iARG.an=="DYms")
-	include(string("InputFiles\\ARGS_",iARG.mm,"_DYms_",iARG.ex,".jl"))	# Parameters to vary
-	open(string("OUT_DYms_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
-		writedlm(io, [vcat([string(i) for i in keys(pN)],10 .^ collect(pert.r[1]:pert.s:pert.r[2]))],'\t')
-		for pI = pN
-			for i = pI[2]
-				p = copy(pO);
-				p[pI[1]] *= (10. ^i);
-				writedlm(io, [vcat([p[i[1]] for i in pN],fn.DYc(p, pert, mm,x0FB,x0NF))],'\t');
-			end
-		end
-	end
-# Optimize CoRa curve for a range of parameters:
-elseif(iARG.an=="OptDY")
-	include(string("InputFiles\\ARGS_",iARG.mm,"_OptDY_",iARG.ex,".jl"))
-	open(string("OUT_OptDY_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
-		if(mrw.prtD==1)
-			writedlm(io, [vcat("Run","Iteration",[string(param) for param in mrw.pOp],string("|DY<=",pert.eps,"|"),"min(DY)",10 .^ collect(pert.r[1]:pert.s:pert.r[2]))], '\t')
-		else
-			writedlm(io, [vcat("Run","Iteration",[string(param) for param in mrw.pOp],string("|DY<=",pert.eps,"|"),"min(DY)")], '\t')
-		end
-		for ruN in 1:mrw.runs
-			println("RUN #",ruN)
-			p = copy(pO);
-			####### Uncomment the next three lines for random initial conditions: #######
-			#for i in 1:length(mrw.pOp)
-			#	p[mrw.pOp[i]] = 10 .^ (rand(Uniform(mrw.pMin[i], mrw.pMax[i])));
-			#end
-			#############################################################################
-			## Temperature function for simulated annealing:
-			if(mrw.temp==1)
-				mrwT = collect(mrw.iter:-1:1) ./ mrw.iter;
-			else
-				mrwT = ones(mrw.iter); # NOTE: For MRW, make T=1.
-			end
-			## Initialize system
-			DY0 = fn.DYc(p, pert, mm,x0FB,x0NF);    # Calculate DY curve
-			DYm = fn.DYm(DY0, pert);    # Calculate metrics of DY curve
-			op0 = log10(DYm[3]/DYm[2]);   # Property to optimize (e.g. DY<=eps range length)
-			mi0 = DYm[4];                 # Secondary property to optimize (e.g. min(DY) value)
-			r0 = zeros(length(mrw.pOp));
-			if(mrw.prtD==1)
-				writedlm(io, [vcat(ruN,0,[p[i] for i in mrw.pOp],op0,mi0,DY0)],'\t')
-			else
-				writedlm(io, [vcat(ruN,0,[p[i] for i in mrw.pOp],op0,mi0)],'\t')
-			end
-			# Optimization iterations
-			println("I: minDY = ",mi0,"\t |DY| = ",op0)
-			for i in 1:mrw.iter
-				rI = rand(MvNormal(zeros(length(mrw.pOp)), zeros(length(mrw.pOp)) .+ mrw.cov)); # Random values to update parameters
-				for pI in 1:length(mrw.pOp)  # Update parameter values
-					r0[pI] = p[mrw.pOp[pI]]; # Save previous value
-					p[mrw.pOp[pI]] *= (mrw.M .^ rI[pI]); # Update value
-					# Exclude values outside regime of exploration:
-					if p[mrw.pOp[pI]] < (10.0 ^ mrw.pMin[pI])
-						p[mrw.pOp[pI]] = (10.0 ^ mrw.pMin[pI])
-					elseif p[mrw.pOp[pI]] > (10.0 ^ mrw.pMax[pI])
-						p[mrw.pOp[pI]] = (10.0 ^ mrw.pMax[pI])
-					end
-				end
-				DYs = fn.DYc(p, pert, mm,x0FB,x0NF);  # Calculate new DY curve
-				DYm = fn.DYm(DYs, pert);  # Calculate new metrics of DY curve
-				op1 = log10(DYm[3]/DYm[2]); # New value of property to optimize (e.g. DY<=eps range length)
-				mi1 = DYm[4];               # New value of secondary property to optimize (e.g. min(DY) value)
-				# Evaluate if accept new parameter values or not:
-				## Only accept in the regime of interest, i.e. DY>=0:
-				c1 = (mi1>=0);
-				## If DY>eps for all rho, evaluate the min(DY) for both sets:
-				### NOTE: As mi0,mi1=[0,1], correct exponential with the expected variance of ~U(0,1)
-					xiC = (mi0 ^ 2) / (2 * 0.083);
-					xiP = (mi1 ^ 2) / (2 * 0.083);
-				c2 = isnan(op0+op1) && (rand() < exp((xiC - xiP) / mrwT[i]));
-				## If DY>=eps for some rho, evaluate the |DY<=eps| for both sets:
-				### NOTE: As op0,op1=[0,rrO], but still correct exponential with the expected variance of ~U(0,1)
-				###       !! ~U(0,1)*(rrO^2) variance resulted in very noisy runs...
-					rrO = pert.r[2] - pert.r[1];
-					xiC = (rrO - op0) / (2 * 0.083);
-					xiP = (rrO - op1) / (2 * 0.083);
-				c3 = rand() < exp((xiC - xiP) / mrwT[i]);
-				if(c1 && (c2 || c3))
-					# If yes, update "reference" system
-					op0 = op1;
-					mi0 = mi1;
-					DY0 = DYs;
-				else
-					# If not, revert to previous parameter values
-					for pI in 1:length(mrw.pOp)
-						p[mrw.pOp[pI]] = r0[pI];
-					end
-				end
-				if(mrw.prtW==1 || i==mrw.iter)
-					if(mrw.prtD==1)
-						writedlm(io, [vcat(ruN,i,[p[i] for i in mrw.pOp],op0,mi0,DY0)],'\t')
-					else
-						writedlm(io, [vcat(ruN,i,[p[i] for i in mrw.pOp],op0,mi0)],'\t')
-					end
-				end
-				if(op1==rrO)
-					println("Optimal value (|DY<=eps|=",op1,") reached at iteration ",i)
-					if(mrw.prtD==1)
-						writedlm(io, [vcat(ruN,i,[p[i] for i in mrw.pOp],op0,mi0,DY0)],'\t')
-					else
-						writedlm(io, [vcat(ruN,i,[p[i] for i in mrw.pOp],op0,mi0)],'\t')
-					end
-					break;
-				end
-			end
-			println("F: minDY = ",mi0,"\t |DY| = ",op0,"\n")
-		end
-	end
 else
-	println("ERROR: Undetermined analysis. Options: ExSSs, ExDyn, DYms, OptDY")
+	println("ERROR: Undetermined analysis. Options: ExDyn")
 end
