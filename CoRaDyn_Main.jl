ec4ef371bc0d383b0280d5faa6b66b664a4aa7d6
mgschiavon <mgschiavon@gmail.com>
2021-11-12 14:54:45 -0600

    Adding msDyn analysis
    For a range of values

--- CoRaDyn_Main.jl
+++ CoRaDyn_Main.jl
@@ -23,9 +23,9 @@ pO = copy(p);
 ## Run analysis
 # Calculate dynamic response after a perturbation:
 if(iARG.an=="ExDyn")
-	p = copy(pO);
 	open(string("OUT_ExDyn_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
-		writedlm(io, [vcat("time","Y","Ynf","CoRa")],'\t');
+		writedlm(io, [vcat("time","Y","nyDf","CoRa")],'\t');
+		p = copy(pO);
 		# Start in steady state conditions
 		fsR, nsR = fn.RefSS(mm,p,pert,x0FB,x0NF);
 		rtol = 1e-12;
@@ -37,12 +37,54 @@ if(iARG.an=="ExDyn")
 		nsD = fn.SS(mm.odeNF, p, nsR, rtol);
 		p[pert.p] /= pert.d;
 		# Calculate and print CoRa:
-		writedlm(io, [vcat(0,mm.outFB(fsR),mm.outNF(nsR),1)],'\t');
+		fyR = mm.outFB(fsR);
+		nyR = mm.outNF(nsR);
+		writedlm(io, [vcat(0,fyR,nyR,1)],'\t');
 		for i in collect(0.1:0.1:10000.0)
-			writedlm(io, [vcat(i,mm.outFB(fdD(i)),mm.outNF(ndD(i)),fn.CoRa(mm.outFB(fsR),mm.outFB(fdD(i)),mm.outNF(nsR),mm.outNF(ndD(i))))],'\t');
+			fyD = mm.outFB(fdD(i));
+			nyD = mm.outNF(ndD(i));
+			writedlm(io, [vcat(i,fyD,nyD,fn.CoRa(fyR,fyD,nyR,nyD))],'\t');
 		end
-		writedlm(io, [vcat(Inf,mm.outFB(fsD),mm.outNF(nsD),fn.CoRa(mm.outFB(fsR),mm.outFB(fsD),mm.outNF(nsR),mm.outNF(nsD)))],'\t');
+		fyF = mm.outFB(fsD);
+		nyF = mm.outNF(nsD);
+		writedlm(io, [vcat(Inf,fyF,nyF,fn.CoRa(fyR,fyF,nyR,nyF))],'\t');
+	end
+elseif(iARG.an=="msDyn")
+	open(string("OUT_msDyn_",iARG.mm,"_",iARG.ex,"_",iARG.pp,"_",iARG.ax,".txt"), "w") do io
+		writedlm(io, [vcat("theta","time","Y","nyDf","CoRa")],'\t');
+		# Range of parameters to explore:
+		r = 10 .^ collect(pert.r[1]:pert.s:pert.r[2]);
+        for i in 1:length(r)
+			p = copy(pO);
+			# Condition (theta) to evaluate:
+			p[pert.c] *= r[i];
+			# Start in steady state conditions
+			fsR, nsR = fn.RefSS(mm,p,pert,x0FB,x0NF);
+			rtol = 1e-12;
+			# Simulate dynamic response after the perturbation:
+			p[pert.p] *= pert.d;
+			fdD = fn.Dyn(mm.odeFB, p, fsR, 100000.0, rtol);
+			fsD = fn.SS(mm.odeFB, p, fsR, rtol);
+			ndD = fn.Dyn(mm.odeNF, p, nsR, 100000.0, rtol);
+			nsD = fn.SS(mm.odeNF, p, nsR, rtol);
+			p[pert.p] /= pert.d;
+			# Calculate and print CoRa:
+			fyR = mm.outFB(fsR);
+			nyR = mm.outNF(nsR);
+			writedlm(io, [vcat(p[pert.c],0,fyR,nyR,1)],'\t');
+			fyF = mm.outFB(fsD);
+			nyF = mm.outNF(nsD);
+			for i in collect(0.1:0.1:10000.0)
+				fyD = mm.outFB(fdD(i));
+				nyD = mm.outNF(ndD(i));
+				writedlm(io, [vcat(p[pert.c],i,fyD,nyD,fn.CoRa(fyR,fyD,nyR,nyD))],'\t');
+				if(max(abs(fyD-fyF),abs(nyD-nyF))<1e-8)
+					break;
+				end
+			end
+			writedlm(io, [vcat(p[pert.c],Inf,fyF,nyF,fn.CoRa(fyR,fyF,nyR,nyF))],'\t');
+		end
 	end
 else
-	println("ERROR: Undetermined analysis. Options: ExDyn")
+	println("ERROR: Undetermined analysis. Options: ExDyn, msDyn")
 end
