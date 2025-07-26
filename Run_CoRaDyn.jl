c5ccffe22668b33d2624759afa3842ca5c28032e
Mariana Gómez-Schiavon, PhD <mgschiavon@users.noreply.github.com>
2025-07-25 18:50:29 -0600

    Fig03A

--- Run_CoRaDyn.jl
+++ Run_CoRaDyn.jl
@@ -6,8 +6,8 @@
 
        Pkg.activate(".");		    # Activate local environment (requiere '.toml' files)
 	iARG = (mm = "PID",             # Label for motif file
-       ex = "Ex01",                    # Label for parameters file
+       ex = "Fig03A",                    # Label for parameters file
        pp = :bC,                       # Label for perturbation type
-       ax = [:bD],                 # Label for condition/environment [:bP, :bD]
+       ax = [],                 # Label for condition/environment [:bP, :bD]
        an = "ExDyn");        # Chose analysis type (Options: ExSSs, ExDyn, CoRams, OptDY)
        include("./CoRaDyn_Main_v1.jl");
